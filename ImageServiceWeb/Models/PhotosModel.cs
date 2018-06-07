d75509a8528620aaa5d2d30ffdb27bd0afc5a5dc
SimSteve <simgutfreund@gmail.com>
2018-06-07 13:55:44 +0300

    fixed a bug

--- ImageServiceWeb/Models/PhotosModel.cs
+++ ImageServiceWeb/Models/PhotosModel.cs
@@ -12,11 +12,15 @@ namespace ImageServiceWeb.Models
             get
             {
                 List<Photo> list = new List<Photo>();
-                string[] pics = Directory.GetFiles(this.configuration.OutputDir + @"\Thumbnails", "*.*", SearchOption.AllDirectories);
-                for (int i = 0; i < pics.Length; i++)
+                try
                 {
-                    list.Add(new Photo(pics[i]));
+                    string[] pics = Directory.GetFiles(this.configuration.OutputDir + @"\Thumbnails", "*.*", SearchOption.AllDirectories);
+                    for (int i = 0; i < pics.Length; i++)
+                    {
+                        list.Add(new Photo(pics[i]));
+                    }
                 }
+                catch { }
                 return list;
             }
         }
